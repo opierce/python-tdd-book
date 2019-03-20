d4a7a8574ff62b62a68053bd52488608b0364dbe
Owen Pierce <opierce@gmail.com>
2019-03-20 16:08:05 -0700

    Adding Lists with migrations

--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.test import TestCase
 from django.urls import resolve
 from django.template.loader import render_to_string
-from models import Item
+from models import Item, List
 
 from lists.views import home_page
 
@@ -34,24 +34,33 @@ class ListViewTest(TestCase):
 		self.assertTemplateUsed(response, 'list.html')
 
 	def test_displays_all_items(self):
-		Item.objects.create(text='itemey1')
-		Item.objects.create(text='itemey2')
+		list_ = List.objects.create()
+		Item.objects.create(text='itemey1', list=list_)
+		Item.objects.create(text='itemey2', list=list_)
 
 		response = self.client.get('/lists/the-only-list-in-the-world/')
 
 		self.assertContains(response, 'itemey1')
 		self.assertContains(response, 'itemey2')
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
 	def test_saving_and_retrieving_items(self):
+		list_ = List()
+		list_.save()
+
 		first_item = Item()
 		first_item.text = 'The first ever list item'
+		first_item.list = list_
 		first_item.save()
 
 		second_item = Item()
 		second_item.text = 'Item the second'
+		second_item.list = list_
 		second_item.save()
+
+		saved_list = List.objects.first()
+		self.assertEqual(saved_list, list_)
 
 		saved_items = Item.objects.all()
 		self.assertEqual(saved_items.count(), 2)
@@ -59,7 +68,9 @@ class ItemModelTest(TestCase):
 		first_saved_item = saved_items[0]
 		second_saved_item = saved_items[1]
 		self.assertEqual(first_saved_item.text, 'The first ever list item')
+		self.assertEqual(first_saved_item.list, list_)
 		self.assertEqual(second_saved_item.text, 'Item the second')
+		self.assertEqual(second_saved_item.list, list_)
 
 
 
