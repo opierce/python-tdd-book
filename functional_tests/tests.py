c4a0d1beb7e2c0ff4f964152d8a780fab68e0a97
Owen Pierce <opierce@gmail.com>
2019-03-20 15:21:54 -0700

    add a new URL, new template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,7 +4,7 @@ from django.test import LiveServerTestCase
 import time
 from selenium.webdriver.common.keys import Keys
 
-MAX_WAIT = 10
+MAX_WAIT = 3
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -71,7 +71,7 @@ class NewVisitorTest(LiveServerTestCase):
 	def test_multiple_users_can_start_lists_at_different_urls(self):
 		# Edith starts a new to-do list
 		self.browser.get(self.live_server_url)
-		inputbox = find_element_by_id('id_new_item')
+		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Buy peacock feathers')
 		inputbox.send_keys(Keys.ENTER)
 		self.wait_for_row_in_list_table('1: Buy peacock feathers')
