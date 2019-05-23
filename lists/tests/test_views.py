50ac9274173702e2cd06ee391da22b0073422657
Owen Pierce <opierce@gmail.com>
2019-05-23 15:06:27 -0700

    Create existing list form

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -128,6 +128,19 @@ class ListViewTest(TestCase):
 		self.assertIsInstance(response.context['form'], ItemForm)
 		self.assertContains(response, 'name="text"')
 
+	def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+		list1 = List.objects.create()
+		item1 = Item.objects.create(list=list1, text='textey')
+		response = self.client.post(
+			f'/lists/{list1.id}/',
+			data={'text': 'textey'}
+		)
+
+		expected_error = escape("You've already got this in your list")
+		self.assertContains(response, expected_error)
+		self.assertTemplateUsed(response, 'list.html')
+		self.assertEqual(Item.objects.all().count(), 1)
+
 class NewItemTest(TestCase):
 
 	def test_can_save_a_POST_request_to_an_existing_list(self):
